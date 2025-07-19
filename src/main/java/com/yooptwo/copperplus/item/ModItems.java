178fef2f4d497768e42bc3cb81890b95942849e7
Yoop233 <siki666@126.com>
2025-07-19 20:50:22 +0800

    first item

---
+++ src/main/java/com/yooptwo/copperplus/item/ModItems.java
@@ -0,0 +1,21 @@
+package com.yooptwo.copperplus.item;
+
+import com.yooptwo.copperplus.CopperPlus;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    //定义了DeferredRegister.Items的实例，用于延迟注册ModItems
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CopperPlus.MODID);
+
+    public static final DeferredItem<Item> ANCIENT_COPPER_INGOT = ITEMS.register("ancient_copper_ingot",
+            () -> new Item(new Item.Properties()));
+
+    //定义一个用于注册物品的方法，参数使用IEventBus
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
